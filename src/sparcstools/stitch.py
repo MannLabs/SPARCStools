451ef1c1f7e1260ee24f582b32e32b181b1efda1
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2024-05-21 15:07:03 +0200

    fix channel lookup to work with channel names

--- src/sparcstools/stitch.py
+++ src/sparcstools/stitch.py
@@ -238,7 +238,7 @@ class Stitcher:
 
             channels = []
             for channel in self.channel_order:
-                self.channels.append(self.channels.index(channel))
+                self.channels.append(self.channel_names.index(channel))
             print("new channel order", channels)
 
             self.channels = channels
